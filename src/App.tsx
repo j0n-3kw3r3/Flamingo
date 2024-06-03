dcd7413bcfe9c7247c2523647913292a4e5ced5f
j0n-3kw3r3 <johnebong950@gmail.com>
2024-06-03 13:08:01 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+        <Button className=' bg-green-500' >Click me</Button>   
+    </>
+  );
+}
+
+export default App
