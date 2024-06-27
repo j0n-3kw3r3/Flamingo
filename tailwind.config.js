35f9d97fe67b9c14041fe0245f4b262884f20a2b
EmediongPeter <emedionguseh18@gmail.com>
2024-06-27 22:39:30 +0100

    articles ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,6 +63,9 @@ module.exports = {
           to: { height: "0" },
         },
       },
+      objectPosition: {
+        'top-left': 'top left',
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
